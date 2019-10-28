a2d774b833104cdf6084a3fd557e7c4c71522679
AngelineBrocq <38132631+AngelineBrocq@users.noreply.github.com>
2019-10-28 01:27:28 +0100

    Update utils.hpp

--- main/include/utils.hpp
+++ main/include/utils.hpp
@@ -4,37 +4,37 @@
 #include <type_traits>
 
 template<typename E>
-constexpr auto to_integral(E e) -> typename std::underlying_type<E>::type 
+constexpr auto to_integral(E e) -> typename std::underlying_type<E>::type
 {
-   return static_cast<typename std::underlying_type<E>::type>(e);
+	return static_cast<typename std::underlying_type<E>::type>(e);
 }
 
 enum class PlatformEndianness
 {
-    BigEndian,
-    LittleEndian
+	BigEndian,
+	LittleEndian
 };
 
 constexpr PlatformEndianness DetectEndianness()
 {
-    union {
-        uint32_t i;
-        char c[4];
-    } testEndian = {0x01020304};
+	union {
+		uint32_t i;
+		char c[4];
+	} testEndian = { 0x01020304 };
 
-    return (testEndian.c[0] == 1)? PlatformEndianness::BigEndian:PlatformEndianness::LittleEndian;
+	return (testEndian.c[0] == 1) ? PlatformEndianness::BigEndian : PlatformEndianness::LittleEndian;
 }
 
 template <typename T>
-T SwapEndian(const T &val) 
+T SwapEndian(const T& val)
 {
-    union U 
-    {
-        std::remove_const<T>::type val;
-        std::array<std::uint8_t, sizeof(T)> raw;
-    } src, dst;
+	union U
+	{
+		typename std::remove_const<T>::type val;
+		std::array<std::uint8_t, sizeof(T)> raw;
+	} src, dst;
 
-    src.val = val;
-    std::reverse_copy(src.raw.begin(), src.raw.end(), dst.raw.begin());
-    return dst.val;
+	src.val = val;
+	std::reverse_copy(src.raw.begin(), src.raw.end(), dst.raw.begin());
+	return dst.val;
 }
